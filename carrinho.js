3a3f3b6ec8ef239577a386c54bf98057262b5b6e
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2024-11-15 15:02:46 -0300

    mensagem

--- carrinho.js
+++ carrinho.js
@@ -2,12 +2,11 @@ const Produto = require('./produto.js');;
 
 class Carrinho extends Produto{
     itens;
-    soma;
-
-    constructor(nome, preco, quantidade, soma){
+    
+    constructor(nome, preco, quantidade){
         super (nome, preco, quantidade);
         this.itens = [];
-        this.soma = soma;
+        
     }
 
     AdicionarCarrinho(item){
@@ -27,22 +26,22 @@ class Carrinho extends Produto{
                 item.MostrarProduto();
             });
         }
-    } SomaQuantidade(soma){
+    } SomaQuantidade(soma = 0){
         console.log("A soma da quantidade dos produtos: ");
         if (this.itens.length === 0) {
             console.log("Sua lista está vazia! não tem como somar os valores e os produtos. :/");
-        } else {
-            Object.values(this.itens).forEach(item => {
-                soma = soma+item;
+            return;
+        }
+        let soma = 0;
+        
+           this.itens.forEach(item => {
+                soma = soma+ item.quantidade;
                 console.log(soma);
-                
-            });
-        }
-        }
+            })
 
    
     
-}
+}}
 
 
 module.exports = Carrinho;
