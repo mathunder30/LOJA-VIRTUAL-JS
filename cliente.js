dc45040b6e2ddb36405fe6bf9c10b970eb34e689
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2024-11-19 11:18:56 -0300

    mensagem

--- cliente.js
+++ cliente.js
@@ -2,7 +2,7 @@ const Jogo = require('./jogo.js');;
 const Carrinho = require('./carrinho.js');;
 const Livro = require('./livro.js');;
 
-//CREATE
+
 const produto1 = new Livro ("Livro", 59.99,1,1,"Sthepen King", "Outsider");
 const produto2 = new Livro("Livro", 87.20, 1,2, "Edécio Fernando lepsen", "Lógica de Programação e Algoritmos com JavaScript - 2ª Edição: Uma introdução à programação de computadores com exemplos e exercícios para iniciantes");
 const produto3 = new Jogo("Jogo", 299.90, 1,3,"Elden Ring", "Steam" )
